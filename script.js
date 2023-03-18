e5563fea988f2c36c025e66b48f77a5a1cb17428
Hamidreza Ahmadi <127620399+hushra@users.noreply.github.com>
2023-03-18 16:48:46 +0330

    Create

---
+++ script.js
@@ -0,0 +1,7 @@
+let emojis = document.body.innerHTML.match(/([\uD800-\uDBFF][\uDC00-\uDFFF])/g);
+if (emojis) {
+    emojis.forEach((emoji) => {
+        let imgTag = '<img draggable="false" style="image-rendering:-webkit-crisp-edges;max-width: 1.5em;max-height: 1.5em;" src="https://raw.githubusercontent.com/hushra/amoji/main/emojis/' + encodeURIComponent(emoji) + '.png" alt="' + emoji + '">';
+        document.body.innerHTML = document.body.innerHTML.replace(new RegExp(emoji, "g"), imgTag);
+    });
+}
