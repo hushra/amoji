41cc8d675fe65247a78b5a05d10d36485eb952cc
Hamidreza Ahmadi <127620399+hushra@users.noreply.github.com>
2023-03-18 17:31:59 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let emojis = document.body.innerHTML.match(/([\uD800-\uDBFF][\uDC00-\uDFFF])/g);
-if (emojis) {
-    emojis.forEach((emoji) => {
-        let imgTag = '<img draggable="false" style="image-rendering:-webkit-crisp-edges;max-width: 1.4em;max-height: 1.4em;" src="https://raw.githubusercontent.com/hushra/amoji/main/emojis/' + encodeURIComponent(emoji) + '.png" alt="' + emoji + '">';
-        document.body.innerHTML = document.body.innerHTML.replace(new RegExp(emoji, "g"), imgTag);
-    });
+class amoji {
+  constructor(element) {
+    let emojis = element.innerHTML.match(/([\uD800-\uDBFF][\uDC00-\uDFFF])/g);
+    if (emojis,size=1.4) {
+        emojis.forEach((emoji) => {
+            let imgTag = '<img draggable="false" style="image-rendering:-webkit-crisp-edges;max-width: '+size+'em;max-height: '+size+'em;" src="https://raw.githubusercontent.com/hushra/amoji/main/emojis/' + encodeURIComponent(emoji) + '.png" alt="' + emoji + '">';
+            document.body.innerHTML = document.body.innerHTML.replace(new RegExp(emoji, "g"), imgTag);
+        });
+    }
+  }
 }
